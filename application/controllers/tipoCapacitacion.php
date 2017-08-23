0f697b59ef7fc112acc44b1bed42a56e6e6a602c
MarthaL <30940521+MarthaL@users.noreply.github.com>
2017-08-23 11:18:32 -0600

    Prueba2.2
    TipoCapacitacitacion

---
+++ application/controllers/tipoCapacitacion.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); //SIEMPRE PEGAR ESTO EN TODOS LOS CONTROLADORES
+
+class TipoCapacitacion extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url'); //llamar el directorio raiz
+		$this->load->database();
+		$this->load->library('Grocery_CRUD');
+	}
+	
+
+
+	public function tipoCap()
+	{
+		$this->load->helper('url'); //llamar el directorio raiz
+		$crud = new Grocery_CRUD();
+
+		$crud->set_table('tb_tipocapacitacion'); //nombre de la tabla q voy a setear
+		$crud->set_theme('bootstrap');
+
+		$crud->set_language("spanish");
+		$output = $crud->render();//renderiza la info de la tabla
+
+		//hacer todo de aqui pa arriba-- todo se hace en el controlador
+		
+		$this->load->view('Master/tipoCapacitacion', $output); //el output hace los sp automatico de insertar y eliminar..
+
+
+
+	}
+} 
+
+
