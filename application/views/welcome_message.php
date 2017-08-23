01d3e3c0225dab94691a04de27502096510f649b
MarthaL <30940521+MarthaL@users.noreply.github.com>
2017-08-23 09:33:59 -0600

    Prueba2.1
    Pruebas

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -53,34 +53,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	 ?>
 -->
 
-<<<<<<< HEAD
+
 <?php $this->load->view('Vistas_Parciales/_navbar'); ?> <!-- llama la vista parcial del navbar-->
-=======
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
->>>>>>> 59b23dcd2d483b728883c2226b956dcd816a45cb
+
+
+
 
 <div id="myCarousel" class="carousel slide" data-ride="carousel" >
     <!-- Indicators -->
